a3bddd5057d2bd219c167fe399767363154d1ab6
march213 <jane.molodetskaya@gmail.com>
2023-08-06 12:23:39 -0400

    clean up

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,27 +1,29 @@
-import { ErrorFallbackProps, ErrorComponent, ErrorBoundary, AppProps } from "@blitzjs/next"
-import { AuthenticationError, AuthorizationError } from "blitz"
 import React from "react"
+import { AuthenticationError, AuthorizationError } from "blitz"
+import { ErrorFallbackProps, ErrorComponent, ErrorBoundary, AppProps } from "@blitzjs/next"
 import { withBlitz } from "src/blitz-client"
 import "src/styles/globals.css"
 
 function RootErrorFallback({ error }: ErrorFallbackProps) {
   if (error instanceof AuthenticationError) {
     return <div>Error: You are not authenticated</div>
-  } else if (error instanceof AuthorizationError) {
+  }
+
+  if (error instanceof AuthorizationError) {
     return (
       <ErrorComponent
         statusCode={error.statusCode}
         title="Sorry, you are not authorized to access this"
       />
     )
-  } else {
-    return (
-      <ErrorComponent
-        statusCode={(error as any)?.statusCode || 400}
-        title={error.message || error.name}
-      />
-    )
   }
+
+  return (
+    <ErrorComponent
+      statusCode={(error as any)?.statusCode || 400}
+      title={error.message || error.name}
+    />
+  )
 }
 
 function MyApp({ Component, pageProps }: AppProps) {

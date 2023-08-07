dd62840914ec760858fdbfc89d35aa809c39ed3e
march213 <jane.molodetskaya@gmail.com>
2023-08-07 19:10:31 -0400

    add nicer authentication form

--- src/core/components/UserInfo.tsx
+++ src/core/components/UserInfo.tsx
@@ -1,41 +1,28 @@
-import Link from "next/link";
 import { useCurrentUser } from "src/features/users/hooks/useCurrentUser";
 import logout from "src/features/auth/mutations/logout";
 import { useMutation } from "@blitzjs/rpc";
-import { Routes } from "@blitzjs/next";
 import { Button } from "@mantine/core";
 
 export const UserInfo = () => {
   const currentUser = useCurrentUser();
   const [logoutMutation] = useMutation(logout);
 
-  if (currentUser) {
-    return (
-      <>
-        <Button
-          onClick={async () => {
-            await logoutMutation();
-          }}
-        >
-          Logout
-        </Button>
-        <div>
-          User id: <code>{currentUser.id}</code>
-          <br />
-          User role: <code>{currentUser.role}</code>
-        </div>
-      </>
-    );
-  } else {
-    return (
-      <>
-        <Button component={Link} href={Routes.SignupPage()}>
-          Sign Up
-        </Button>
-        <Button component={Link} href={Routes.LoginPage()}>
-          Login
-        </Button>
-      </>
-    );
-  }
+  if (!currentUser) return null;
+
+  return (
+    <>
+      <Button
+        onClick={async () => {
+          await logoutMutation();
+        }}
+      >
+        Logout
+      </Button>
+      <div>
+        User id: <code>{currentUser.id}</code>
+        <br />
+        User role: <code>{currentUser.role}</code>
+      </div>
+    </>
+  );
 };
--- src/pages/index.page.tsx
+++ src/pages/index.page.tsx
@@ -1,11 +1,21 @@
 import Layout from "src/core/layouts/Layout";
 import { BlitzPage } from "@blitzjs/next";
 import { UserInfo } from "../core/components/UserInfo";
+import { AuthenticationForm } from "src/core/components/MainAuthenticationForm";
+import { useCurrentUser } from "src/features/users/hooks/useCurrentUser";
+import { Vertical } from "mantine-layout-components";
 
 const Home: BlitzPage = () => {
+  const currentUser = useCurrentUser();
+
   return (
     <Layout title="Home">
-      <UserInfo />
+      {currentUser && <UserInfo />}
+      {!currentUser && (
+        <Vertical center fullH fullW>
+          <AuthenticationForm />
+        </Vertical>
+      )}
     </Layout>
   );
 };

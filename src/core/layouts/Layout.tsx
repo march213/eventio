549fa1651834212fffd2640dd4ecf68a33d48852
march213 <jane.molodetskaya@gmail.com>
2023-08-06 22:46:14 -0400

    use layout helpers and app shell

--- src/core/layouts/Layout.tsx
+++ src/core/layouts/Layout.tsx
@@ -1,11 +1,14 @@
-import Head from "next/head";
 import React, { Suspense } from "react";
+import Head from "next/head";
 import { BlitzLayout } from "@blitzjs/next";
+import { AppShell, Navbar, Header, Text, Footer } from "@mantine/core";
+import { Horizontal, Vertical } from "mantine-layout-components";
 
 const Layout: BlitzLayout<{ title?: string; children?: React.ReactNode }> = ({
   title,
   children,
 }) => {
+  const thisYear = new Date().getFullYear();
   return (
     <>
       <Head>
@@ -13,7 +16,40 @@ const Layout: BlitzLayout<{ title?: string; children?: React.ReactNode }> = ({
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
-      <Suspense fallback="Loading...">{children}</Suspense>
+      <AppShell
+        padding="md"
+        // navbar={
+        //   <Navbar width={{ base: 300 }} height={500} p="xs">
+        //     {/* Navbar content */}
+        //   </Navbar>
+        // }
+        header={
+          <Header height={45} p="xs">
+            <Horizontal fullH>
+              <Text fw="bold">Eventio</Text>
+            </Horizontal>
+          </Header>
+        }
+        footer={
+          <Footer height={30}>
+            <Horizontal fullH fullW center>
+              <Text fz="xs" color="dimmed">
+                &copy; copyright {thisYear}
+              </Text>
+            </Horizontal>
+          </Footer>
+        }
+        styles={(theme) => ({
+          main: {
+            backgroundColor:
+              theme.colorScheme === "dark" ? theme.colors.dark[8] : theme.colors.gray[0],
+          },
+        })}
+      >
+        <Vertical fullH fullW>
+          <Suspense fallback="Loading...">{children}</Suspense>
+        </Vertical>
+      </AppShell>
     </>
   );
 };
